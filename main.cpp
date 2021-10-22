8c60757cd64764b60452733881d2c6adece838b0
Kylian <hsjgc1234@gmail.com>
2021-10-22 17:40:26 +0900

    decoding

--- main.cpp
+++ main.cpp
@@ -57,26 +57,47 @@ void DiffManchesterEncoding(const string &code) {
 }
 
 void ManchesterDecoding(const string &code){
+  cout << "Manchester Decoding" << '\n';
   unsigned long len = code.length();
-//  for (int i = 0; i < len; ++i) {
-//
-//  }
-  cout << len << '\n';
+  for (int i = 0; i < len; i += 7) {
+    if(code[i] == '|'){
+      i -= 6;
+      continue;
+    }
+    string temp = code.substr(i, 7);
+    if(temp == "⎺|⎽")
+      cout << '0';
+    else
+      cout << '1';
+  }
+  cout << '\n';
 }
 
 void DiffManchesterDecoding(const string &code){
-
+  cout << "Differential Manchester Decoding" << '\n';
+  unsigned long len = code.length();
+  for (int i = 0; i < len;) {
+    if(code[i] == '|'){
+      cout << '0';
+      i += 8;
+    }
+    else{
+      cout << '1';
+      i += 7;
+    }
+  }
+  cout << '\n';
 }
 
 int main(){
   string code;
   cin >> code;
-  //code = 01100010110101100111
+//  code = 01100010110101100111
   ManchesterEncoding(code);
   DiffManchesterEncoding(code);
 
   string code1 = "⎺|⎽⎽|⎺|⎽|⎺⎺|⎽|⎺|⎽|⎺|⎽⎽|⎺⎺|⎽⎽|⎺|⎽|⎺⎺|⎽⎽|⎺⎺|⎽⎽|⎺|⎽|⎺⎺|⎽|⎺|⎽⎽|⎺|⎽|⎺|⎽|⎺";
-//  string code1 = "⎺";
+//  string code1 = "⎽|⎺";
   string code2 = "|⎺|⎽⎽|⎺⎺|⎽|⎺|⎽|⎺|⎽|⎺|⎽⎽|⎺|⎽|⎺⎺|⎽⎽|⎺|⎽|⎺⎺|⎽|⎺|⎽⎽|⎺⎺|⎽|⎺|⎽|⎺|⎽⎽|⎺⎺|⎽⎽|⎺";
   ManchesterDecoding(code1);
   DiffManchesterDecoding(code2);
